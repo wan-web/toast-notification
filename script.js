fc2ed54fe8f7485f1a8d1a3aa17b357387e7cd19
wan-web <122721503+wan-web@users.noreply.github.com>
2023-03-01 08:17:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const buttons = document.querySelectorAll('.buttons .btn');
+const notifications = document.querySelector('.notifications');
+
+const toastDetails = {
+  timer: 5000,
+  success: {
+    icon: "fa-circle-check",
+    text: "Success: This is a success toast."
+  },
+  error: {
+    icon: "fa-xmark",
+    text: "Error: This is an error toast."
+  },
+  warning: {
+    icon: "fa-triangle-exclamation",
+    text: "Warning: This is a warning toast."
+  },
+  info: {
+    icon: "fa-circle-info",
+    text: "Info: This is an information toast"
+  }
+}
+
+const removeToast = (toast) => {
+  toast.classList.add('hide');
+  if(toast.timeoutId) clearTimeout(toast.timeoutId);// Clearing the timeourt for the toast
+  setTimeout( () => toast.remove(), 500);
+}
+
+const createToast = (id) => {
+  const {icon, text} = toastDetails[id];
+  const toast = document.createElement('li');
+  toast.className = `toast ${id}`;
+  toast.innerHTML = `
+    <div class="column">
+      <i class="fa-solid ${icon}"></i>
+      <span>${text}</span>
+    </div>
+    <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>
+  `;
+  notifications.appendChild(toast);
+  toast.timeoutId = setTimeout( () => removeToast(toast), toastDetails.timer);
+}
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    createToast(btn.id);
+  })
+})
+
